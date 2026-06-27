c32cc4b3c2ebf431d3bdb17acfd7dd67748b799f
Syed-Ashiq-AP <syedashiq.ap@gmail.com>
2026-06-27 17:14:22 +0530

    temp - test - 2

--- src/pages/organizerEvents.tsx
+++ src/pages/organizerEvents.tsx
@@ -126,7 +126,7 @@ export const OrganizerEvents = () => {
                   <Scanner
                     onScan={async (result) => {
                       const id = result.at(0)?.format;
-                      alert(result);
+                      alert(JSON.stringify(result));
                       if (!id) {
                         toast.error("Failed to scan!");
                         return;
